5611fd1858e50c8f1aaf59a4631918a41d52d8e7
dasiiphone92-prog <dasiiphone92@gmail.com>
2025-12-17 19:46:07 +0200

    Update api.js

--- front/src/api.js
+++ front/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://gmach-project1.onrender.com",
 });
 
 // מוסיף אוטומטית את הטוקן לכל בקשה
